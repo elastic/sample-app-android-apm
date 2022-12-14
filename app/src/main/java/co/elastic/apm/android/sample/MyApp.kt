2cad839629dc15ca2acba4bd41a31bf40e99c225
Cesar Munoz <cesar.munozquintero@elastic.co>
2022-12-14 17:44:08 +0100

    Revert "Initializing the Elastic APM agent"
    This reverts commit 6c3e383b2b7b601c2c08e70a7cb54ee3ce646fac.

--- app/src/main/java/co/elastic/apm/android/sample/MyApp.kt
+++ app/src/main/java/co/elastic/apm/android/sample/MyApp.kt
@@ -1,12 +1,10 @@
 package co.elastic.apm.android.sample
 
 import android.app.Application
-import co.elastic.apm.android.sdk.ElasticApmAgent
 
 class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        ElasticApmAgent.initialize(this)
     }
 }
