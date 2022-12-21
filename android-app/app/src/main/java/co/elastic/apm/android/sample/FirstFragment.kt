4e9f980830a567a12309859757dc2f45b332338a
Alexander Wert <alexander.wert@elastic.co>
2022-12-21 14:28:57 +0100

    Added local city weather backend

---
+++ android-app/app/src/main/java/co/elastic/apm/android/sample/FirstFragment.kt
@@ -0,0 +1,52 @@
+package co.elastic.apm.android.sample
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import co.elastic.apm.android.sample.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            val bundle = bundleOf("city" to binding.citySpinner.selectedItem.toString())
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment, bundle)
+        }
+
+        ArrayAdapter.createFromResource(
+            view.context,
+            R.array.city_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.citySpinner.adapter = adapter
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ backend/src/main/java/co/elastic/apm/android/sample/backend/data/CurrentWeatherResponse.java
@@ -0,0 +1,8 @@
+package co.elastic.apm.android.sample.backend.data;
+
+import lombok.Data;
+
+@Data
+public class CurrentWeatherResponse {
+    double temperature;
+}
